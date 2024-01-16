0f2f66044bfcdf366d2d0a1240a8eecc5a5a7c8f
paroom6 <opwqer3729@gmail.com>
2024-01-16 17:31:41 +0900

    init: project init

--- src/main/java/com/sutdy/servlet_study/servlet/HttpStudyServlet2.java
+++ src/main/java/com/sutdy/servlet_study/servlet/HttpStudyServlet2.java
@@ -1,10 +1,13 @@
 package com.sutdy.servlet_study.servlet;
 
 import java.io.IOException;
+import java.text.CollationKey;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.SortedMap;
 import java.util.TreeMap;
 
 import javax.servlet.ServletException;
@@ -19,49 +22,39 @@ import lombok.Value;
 import lombok.val;
 
 
-@WebServlet("/http")//주소명은 소문자로
-public class HttpStudyServlet extends HttpServlet {
+@WebServlet("/http1")//주소명은 소문자로
+public class HttpStudyServlet2 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
   
-    public HttpStudyServlet() {
+    public HttpStudyServlet2() {
         super();
         
     }
-    //http메소드
-    /*
-     * POST 요청 	: C reate 추가 //GET과 차이, 왜 getParameterMap()은 value로 Sting[]인가?
-     * GET 요청 	: R ead 조회  //GET요청 이외에는 body 사용
-     * PUSH 요청 	: U pdate 수정 
-     * DELETE 요청	: D elete 삭제
-     * 요청메세지는 같으나 메소드에 따라 발생하는 동작이 달라진다.
-     * 
-     * html: 서버를 구성하는 코드
-     * 
-     * CSR 클라이언트 사이드 렌더링
-     * :react
-     * 클라이언트에서 html 을 작성 필요한 데이터를 서버에 요청 데이터를 응답받음
-     * 
-     * SSR 서버 사이드 렌더링
-     * : 서블릿 JSP 
-     * 클라이언트에서 요청을 하면 서버에서 html을 완성 후 html로 응답
-     */
+
     
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException { //client가 백엔드에 요청한 목록 리스트
-    	//한글깨짐 - body로 request를 보낼때 utf-8로 변환 필요 - 필터처리
-    	//request.setCharacterEncoding("utf-8");
     	Map<String, String[]> map = request.getParameterMap(); //키를 명시하지 않고 한 덩어리로 받기 좋다.
-    	 Map<String, String> paramsMap = ParamsConverter.convertParamsMapToMap(map);
-
-    	System.out.println(paramsMap);
-    	System.out.println(request.getParameter("name"));
-    	
-    	Map<String, String> paramsMap2 = new HashMap<>(); 
-    	Iterator<String> ir =  request.getParameterNames().asIterator();//키만 넣어둔것
-    	while(ir.hasNext()) {
-    		String key = ir.next();
-    		paramsMap2.put(key, request.getParameter(key));//key = 맵의 키들
-    		//System.out.println(ir.next());
-    	}
+    	Map<String, String> paramsMap = ParamsConverter.convertParamsMapToMap(map);
+    		
+    	SortedMap<String, String[]> sMap = Collections.synchronizedSortedMap(new TreeMap<String, String[]>(request.getParameterMap()));
+    	synchronized (sMap) {
+    		for(String key : sMap.keySet()) {
+    			String[] value = sMap.get(key);
+    			for(int i = 0; i < value.length; i++) {
+    				System.out.println(key + ": " + value[i]);
+    			}
+    		}
+		}
+//    	System.out.println(paramsMap);
+//    	System.out.println(request.getParameter("name"));
+//    	
+//    	Map<String, String> paramsMap2 = new HashMap<>(); 
+//    	Iterator<String> ir =  request.getParameterNames().asIterator();//키만 넣어둔것
+//    	while(ir.hasNext()) {
+//    		String key = ir.next();
+//    		paramsMap2.put(key, request.getParameter(key));//key = 맵의 키들
+//    		//System.out.println(ir.next());
+//    	}
     	// request.getParameterValues();//값만 넣어둔것
     	
     	//String nameParams = request.getParameter("name"); //요청을 받는 자료형은 Sting 이다. 한글의 경우 마지막 글자가 씹힐 수도 있으니 한번 더 확인
